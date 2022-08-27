12988c57f1baf901c77b5279e3acf360189f185d
rohit4242 <rohitluni4242@gamil.com>
2022-08-28 01:07:56 +0530

    init

--- register.js
+++ register.js
@@ -1,26 +1,31 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+import { getAuth,signInWithEmailAndPassword,createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAv2eXz01yFnv3SYrr0Srah6rcZs5YrvKA",
-    authDomain: "first-project-ce849.firebaseapp.com",
-    databaseURL: "https://first-project-ce849-default-rtdb.firebaseio.com",
-    projectId: "first-project-ce849",
-    storageBucket: "first-project-ce849.appspot.com",
-    messagingSenderId: "192688794206",
-    appId: "1:192688794206:web:708be28fc3c82bd587a2cf"
-  };
-
+    apiKey: "AIzaSyCOQdgJmP3fwrjPZsdzyM6QmN0o56yHMVY",
+    authDomain: "localhost",
+    databaseURL: "https://garbage-collector-13142-default-rtdb.firebaseio.com",
+    projectId: "garbage-collector-13142",
+    storageBucket: "garbage-collector-13142.appspot.com",
+    messagingSenderId: "214211705907",
+    appId: "1:214211705907:web:f22e3dcb4b62964e93f746",
+    measurementId: "G-CGMTVM3TQK"
+};
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const database = getDatabase(app);
-  const auth = getAuth();
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const database = getDatabase(app);
 
-function sing_up(){
+let sing_up = document.getElementById('sign-up-btn');
+let sing_in = document.getElementById('sign-in-btn');
 
-    email = document.getElementById('email').value;
-    password = document.getElementById('password').value;
-    username = document.getElementById('username').value;
+sing_up.addEventListener('click',function (e){
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    let username = document.getElementById('username').value;
 
-    if(validate_email(email) == false || validate_password(password) == false){
+    if(validate_password(password) == false){
         alert('email or password is invalid');
         return
     }
@@ -29,75 +34,123 @@ function sing_up(){
         alert('one more extra fields is empty');
         return
     }
-
-    auth.createUserWithEmailAndPassword(email, password)
-    .then(function() {
-        var user = auth.currentUser;
+    createUserWithEmailAndPassword(email, username, password).then((userCredential) => {
+    // Signed in 
+        const user = userCredential.user;
         var database_ref = database.ref();
-
         var user_data = {
-            email: email,
-            username: username,
-            last_login: Date.now()
-        };
-        database_ref.child('users/'+ user.uid).set(user_data);
+                email: email,
+                username: username,
+                last_login: Date.now()
+            };
 
+        database_ref.child('users/'+ user.uid).set(user_data);
         alert('user created');
 
+    // ...
     })
-    .catch(function(error){
-        var error_code = error.code;
-        var error_message = error.message;
-        alert(error_message);
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+    // ..
     });
-}
+
+})
+
+// function sing_up(){
+
+    // auth.createUserWithEmailAndPassword(email, password)
+    // .then(function() {
+    //     var user = auth.currentUser;
+    //     var database_ref = database.ref();
+
+    //     var user_data = {
+    //         email: email,
+    //         username: username,
+    //         last_login: Date.now()
+    //     };
+    //     database_ref.child('users/'+ user.uid).set(user_data);
+
+    //     alert('user created');
+
+    // })
+    // .catch(function(error){
+    //     var error_code = error.code;
+    //     var error_message = error.message;
+    //     alert(error_message);
+    // });
+// }
 
 // Set up our login function
-function login() {
-    // Get all our input fields
-    email = document.getElementById('email').value
-    password = document.getElementById('password').value
+
+sing_in.addEventListener('click',function (e){
+    let email = document.getElementById('email').value
+    let password = document.getElementById('password').value
   
     // Validate input fields
-    if (validate_email(email) == false || validate_password(password) == false) {
+    if (validate_password(password) == false) {
       alert('Email or Password is Outta Line!!')
       return
       // Don't continue running the code
     }
+
+    signInWithEmailAndPassword(email, password).then((userCredential) => {
+    // Signed in 
+        console.log('ok');
+        const user = userCredential.user;
+        var database_ref = database.ref();
+        var user_data = {
+                last_login : Date.now()
+            }
+        database_ref.child('users/' + user.uid).update(user_data)
+                // DOne
+        alert('User Logged In!!')
+    // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+    });
   
-    auth.signInWithEmailAndPassword(email, password)
-    .then(function() {
-      // Declare user variable
-      var user = auth.currentUser
-      // Add this user to Firebase Database
-      var database_ref = database.ref()
-      // Create User data
-      var user_data = {
-        last_login : Date.now()
-      }
-      // Push to Firebase Database
-      database_ref.child('users/' + user.uid).update(user_data)
-      // DOne
-      alert('User Logged In!!')
-    })
-    .catch(function(error) {
-      // Firebase will use this to alert of its errors
-      var error_code = error.code
-      var error_message = error.message
-      alert(error_message)
-    })
-}
+});
+// function login() {
+    // Get all our input fields
+ 
+    // auth.signInWithEmailAndPassword(email, password)
+    // .then(function() {
+    //   // Declare user variable
+    //   var user = auth.currentUser
+    //   // Add this user to Firebase Database
+    //   var database_ref = database.ref()
+    //   // Create User data
+    //   var user_data = {
+    //     last_login : Date.now()
+    //   }
+    //   // Push to Firebase Database
+    //   database_ref.child('users/' + user.uid).update(user_data)
+    //   // DOne
+    //   alert('User Logged In!!')
+    // })
+    // .catch(function(error) {
+    //   // Firebase will use this to alert of its errors
+    //   var error_code = error.code
+    //   var error_message = error.message
+    //   alert(error_message)
+    // })
+// }
 
-function validate_email(email){
-    expression = /^[^@]+@\w+(\.\w+)+\w$/
+// function validate_email(email){
+//     expression = /^[^@]+@\w+(\.\w+)+\w$/
 
-    if(expression.test(email) == true){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
+//     if(expression.test(email) == true){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
 
 function validate_password(password){
     if(password < 6){
